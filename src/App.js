57e5372b9734bab7481d4bd83cbeb4b0b8594584
Danish Siddiqui <siddiquidn123@gmail.com>
2021-11-10 10:24:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 //class-->className    for-->htmlFor
 import './App1.css';
 import React, { useState } from 'react'
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import Alert from './Components/Alert';
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
@@ -29,29 +30,27 @@ function App() {
       setmode('dark');
       document.body.style.backgroundColor = 'black';
       showalert('Dark mode is enabled', 'success');
-      document.title = 'TextUtils - Dark-Mode';
+      // document.title = 'TextUtils - Dark-Mode';
     }
     else {
       setmode('light');
       document.body.style.backgroundColor = 'white';
       showalert('Light mode is enabled', 'success');
-      document.title = 'TextUtils - Light-Mode';
+      // document.title = 'TextUtils - Light-Mode';
     }
   }
   return (
     <>
-      {/* <BrowserRouter> */}
+      <BrowserRouter>
         <Navbar title="TextUtils" About="About" mode={mode} toggleStyle={toggleStyle} />
         <Alert alert={alert} />
         <div className="container my-3">
-        {/* <Routes> */}
-          {/* <Route path="/" element={ */}
-            <TextForm showalert={showalert} heading="Enter the text to analyze" mode={mode} />
-          {/* // } /> */}
-          {/* <Route path="About/" element={<About />} /> */}
-        {/* </Routes> */}
+          <Routes>
+            <Route path="/" element={<TextForm showalert={showalert} heading="Enter the text to analyze" mode={mode} />} />
+            <Route path="/About" element={<About mode={mode} />} />
+          </Routes>
         </div>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -32,16 +32,16 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{color:props.mode==='dark'?'white':'black',backgroundColor: props.mode==='dark'?'#3B3B3B':'white'}} id="myBox" rows="12"></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
             <div className="container my-3">
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").length} words {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length} minutes read</p>
-                <h3>Preview</h3>
-                <p>{text}</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
+                <h3>Preview</h3> 
+                <p>{text.length>0?text:"Nothing to preview."}</p>
             </div>
 
         </div>
